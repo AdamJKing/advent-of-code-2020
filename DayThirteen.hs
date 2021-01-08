950f3bf35b6ba954c979a10b8499e55004cca524
aking <adamjking1995@outlook.com>
2021-01-08 13:37:07 +0000

    --wip-- [skip ci]

--- DayThirteen.hs
+++ DayThirteen.hs
@@ -5,6 +5,7 @@
 module DayThirteen where
 
 import Control.Applicative (Alternative ((<|>)))
+import Control.Monad (forM, guard)
 import Data.Attoparsec.ByteString.Char8 (char, decimal)
 import Data.Attoparsec.ByteString.Lazy (Parser, maybeResult, parse, sepBy1)
 import qualified Data.ByteString.Lazy as LB
@@ -15,6 +16,7 @@ import qualified Data.List.NonEmpty as NE
 import Data.Maybe (catMaybes, fromMaybe)
 import Data.Monoid (First (..))
 import Data.Semigroup (All (All, getAll))
+import Data.Traversable (for)
 
 data Notes = Notes {earliest :: Int, buses :: NonEmpty (Maybe Int)}
   deriving (Show)
@@ -37,20 +39,12 @@ waitTime Notes {earliest, buses} = do
     activeBuses = catMaybes (NE.toList buses)
     usable i = (i,) <$> find ((== 0) . (i `mod`)) activeBuses
 
-incByUntil :: Num a => a -> (a -> Bool) -> a
-incByUntil n p = until p (+ n) 0
-
-findTimestamp :: Notes -> Int
-findTimestamp Notes {buses = (Nothing :| _)} = error "invalid bus schedule"
-findTimestamp Notes {buses = (Just bus :| buses)} = incByUntil bus usable
-  where
-    indexed = buses `zip` [1 ..]
-    usable t = getAll $ foldMap allValid indexed
-      where
-        allValid (Nothing, _) = mempty
-        allValid (Just n, ix) = All (t `mod` n == (n - ix))
-
 sample :: LB.ByteString
 sample =
   "939\n\
   \7,13,x,x,59,x,31,19"
+
+-- 0 = ka - (kb + 1) - (kc + 4) - (kd + 6)
+-- 0 = 7k - (13k + 1) - (59k + 4) - (31k + 6) - (19k + 7)
+
+-- 0 = 0 - 1 + 4 - 6 + 7
